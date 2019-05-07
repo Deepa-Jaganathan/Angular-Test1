9168d290f5c66b985041c702e31b3e5509c23db2
Deepa-Jaganathan <deepa2y5@gmail.com>
2019-05-07 17:43:00 -0400

    first commit

---
+++ app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Deepa Jaganthan';
+}
+
+/* let name= {
+  full : 'Deepa',
+  last : 'Jaganathan',
+  printName : function() {
+  console.log('name: ' + this.full +" "+ this.last);
+  }
+}
+name.printName();
+//call borrowing=> calling function from different object
+let name2={
+  full : 'Yojith',
+  last : 'Madhan',
+}
+name.printName.call(name2);
+
+console.log("Plant corn");
+//setTimeout(function() {console.log("Water plant")}, 3000);
+
+console.log("Add fertilizer"); */
+
+
